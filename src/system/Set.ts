9d57f70ee59c8f1fc49728da09c1dc4f9e1be656
Eugene Kosukhin <kosuha606@gmail.com>
2025-06-27 18:30:38 +0300

    feat(49): set component updated

--- src/system/Set.ts
+++ src/system/Set.ts
@@ -1,20 +1,20 @@
-import { sourceAll, SourceType, systemPatron, value } from "silentium";
+import { all, I, Information, O } from "silentium";
 
 /**
  * Ability to mutate some object, helpful when integrate to procedure systems
  * https://silentium-lab.github.io/silentium-components/#/system/set
  */
 export const set = <T extends Record<string, unknown>>(
-  baseSrc: SourceType<T>,
-  keySrc: SourceType<string>,
-  valueSrc: SourceType<unknown>,
+  baseSrc: Information<T>,
+  keySrc: Information<string>,
+  valueSrc: Information<unknown>,
 ) => {
-  value(
-    sourceAll([baseSrc, keySrc, valueSrc]),
-    systemPatron(([base, key, value]) => {
-      (base as Record<string, unknown>)[key] = value;
-    }),
-  );
-
-  return baseSrc;
+  return I((o) => {
+    all(baseSrc, keySrc, valueSrc).value(
+      O(([base, key, value]) => {
+        (base as Record<string, unknown>)[key] = value;
+        o.give(base);
+      }),
+    );
+  });
 };
