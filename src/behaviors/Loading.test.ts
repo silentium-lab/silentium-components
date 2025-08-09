77d28966886f21c75a4dc187625f21c0e3425223
Eugene Kosukhin <kosuha606@gmail.com>
2025-08-09 16:30:30 +0300

    Loading done

--- src/behaviors/Loading.test.ts
+++ src/behaviors/Loading.test.ts
@@ -1,15 +1,15 @@
-import { of, ownerSync } from "silentium";
-import { expect, test } from "vitest";
+import { of } from "silentium";
+import { expect, test, vi } from "vitest";
 import { loading } from "../behaviors/Loading";
 
 test("Loading.test", () => {
   const [loadingStartSource, lso] = of();
   const [loadingFinishSource, lfo] = of();
-  const loadingSrc = ownerSync(
-    loading(loadingStartSource, loadingFinishSource),
-  );
-  lso.give({});
-  expect(loadingSrc.syncValue()).toBe(true);
-  lfo.give({});
-  expect(loadingSrc.syncValue()).toBe(false);
+  const loadingSrc = loading(loadingStartSource, loadingFinishSource);
+  const g = vi.fn();
+  loadingSrc(g);
+  lso({});
+  expect(g).toHaveBeenLastCalledWith(true);
+  lfo({});
+  expect(g).toHaveBeenLastCalledWith(false);
 });
