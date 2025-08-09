9f8d25f2175edf1a53515e9f1c3d27832cbac0d8
Eugene Kosukhin <kosuha606@gmail.com>
2025-08-09 16:50:21 +0300

    fix(pure): lock done

--- src/behaviors/Lock.ts
+++ src/behaviors/Lock.ts
@@ -1,23 +1,19 @@
-import { filtered, Information, O } from "silentium";
+import { filtered, InformationType } from "silentium";
 
 /**
  * https://silentium-lab.github.io/silentium-components/#/behaviors/lock
  */
 export const lock = <T>(
-  baseSrc: Information<T>,
-  lockSrc: Information<boolean>,
-) => {
+  baseSrc: InformationType<T>,
+  lockSrc: InformationType<boolean>,
+): InformationType<T> => {
   let locked = false;
-
   const i = filtered(baseSrc, () => !locked);
 
-  i.executed(() => {
-    lockSrc.value(
-      O((newLock) => {
-        locked = newLock;
-      }),
-    );
-  });
-
-  return i;
+  return (o) => {
+    lockSrc((newLock) => {
+      locked = newLock;
+    });
+    i(o);
+  };
 };
