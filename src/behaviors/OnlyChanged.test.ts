eded1103a9ce6f542d1640773cb200de0aa0c99c
Eugene Kosukhin <kosuha606@gmail.com>
2025-08-10 17:06:27 +0300

    fix(pure): only changed done

--- src/behaviors/OnlyChanged.test.ts
+++ src/behaviors/OnlyChanged.test.ts
@@ -1,18 +1,18 @@
-import { O, of, pool } from "silentium";
+import { of, shared } from "silentium";
 import { expect, test, vi } from "vitest";
 import { onlyChanged } from "../behaviors/OnlyChanged";
 
 test("OnlyChanged.test", () => {
   const [src, so] = of<number>(1);
-  const [changedSrc] = pool(onlyChanged(src));
+  const [changedSrc] = shared(onlyChanged(src));
 
   const g = vi.fn();
-  changedSrc.value(O(g));
+  changedSrc(g);
   expect(g).not.toBeCalled();
 
-  so.give(2);
+  so(2);
 
   const g2 = vi.fn();
-  changedSrc.value(O(g2));
+  changedSrc(g2);
   expect(g2).toBeCalledWith(2);
 });
