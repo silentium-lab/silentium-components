cf8f0d3cc7cd23ecbeabbf27adb28288ee64c919
Eugene Kosukhin <kosuha606@gmail.com>
2025-08-13 22:29:26 +0300

    fix(pure): shot component fixed

--- src/behaviors/Shot._onlyChanged.test.ts
+++ src/behaviors/Shot._onlyChanged.test.ts
@@ -1,36 +1,34 @@
-import { O, of, poolStateless } from "silentium";
+import { of, sharedStateless } from "silentium";
 import { expect, test } from "vitest";
 import { onlyChanged } from "../behaviors/OnlyChanged";
 import { shot } from "../behaviors/Shot";
 
 test("Shot._onlyChanged.test", () => {
   const [baseSrc, bo] = of<number>(123);
-  const [sharedBase] = poolStateless(baseSrc);
+  const [sharedBase] = sharedStateless(baseSrc);
   const resultSrc = shot(sharedBase, onlyChanged(sharedBase));
 
   const vals: number[] = [];
 
-  resultSrc.value(
-    O((v) => {
-      vals.push(v);
-    }),
-  );
+  resultSrc((v) => {
+    vals.push(v);
+  });
 
   expect(vals).toStrictEqual([]);
 
-  bo.give(222);
+  bo(222);
 
   expect(vals).toStrictEqual([]);
 
-  bo.give(222);
+  bo(222);
 
   expect(vals).toStrictEqual([222]);
 
-  bo.give(333);
+  bo(333);
 
   expect(vals).toStrictEqual([222, 333]);
 
-  bo.give(123);
+  bo(123);
 
   expect(vals).toStrictEqual([222, 333, 123]);
 });
--- src/behaviors/Shot.test.ts
+++ src/behaviors/Shot.test.ts
@@ -1,30 +1,29 @@
-import { O, of, ownerSync, poolStateless } from "silentium";
+import { of } from "silentium";
 import { expect, test, vi } from "vitest";
 import { shot } from "../behaviors/Shot";
 
 test("Shot.test", () => {
   const [baseSrc, bo] = of();
   const [shotSrc, so] = of();
-  const [result, ro] = of();
-  const resultSync = ownerSync(result);
 
-  const [shotResult] = poolStateless(shot(baseSrc, shotSrc));
-  shotResult.value(ro);
-
-  bo.give(1);
-  so.give(1);
-
-  expect(resultSync.syncValue()).toBe(1);
-
-  bo.give(2);
-
-  expect(resultSync.syncValue()).toBe(1);
-
-  so.give(1);
-
-  expect(resultSync.syncValue()).toBe(2);
-
+  const shotResult = shot(baseSrc, shotSrc);
   const g = vi.fn();
-  shotResult.value(O(g));
-  expect(g).not.toBeCalled();
+  shotResult(g);
+
+  bo(1);
+  so(1);
+
+  expect(g).toHaveBeenLastCalledWith(1);
+
+  bo(2);
+
+  expect(g).toHaveBeenLastCalledWith(1);
+
+  so(1);
+
+  expect(g).toHaveBeenLastCalledWith(2);
+
+  const g2 = vi.fn();
+  shotResult(g2);
+  expect(g2).toHaveBeenLastCalledWith(2);
 });
