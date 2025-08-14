77bd06991a1a626f6c5c681d12ebabb996dcefbb
Eugene Kosukhin <kosuha606@gmail.com>
2025-08-14 21:56:03 +0300

    fix(pure): regexpMatch done

--- src/system/RegexpMatch.ts
+++ src/system/RegexpMatch.ts
@@ -1,19 +1,22 @@
-import { all, I, Information, O } from "silentium";
+import { all, i, InformationType } from "silentium";
 
 /**
  * First match of regexp
  * https://silentium-lab.github.io/silentium-components/#/system/regexp-matched
  */
-export const regexpMatch = (
-  patternSrc: Information<string>,
-  valueSrc: Information<string>,
-  flagsSrc: Information<string> = I(""),
-): Information<string[]> =>
-  I((o) => {
-    all(patternSrc, valueSrc, flagsSrc).value(
-      O(([pattern, value, flags]) => {
-        const result = new RegExp(pattern, flags).exec(value);
-        o.give(result ?? []);
-      }),
-    );
-  });
+export const regexpMatch =
+  (
+    patternSrc: InformationType<string>,
+    valueSrc: InformationType<string>,
+    flagsSrc: InformationType<string> = i(""),
+  ): InformationType<string[]> =>
+  (o) => {
+    all(
+      patternSrc,
+      valueSrc,
+      flagsSrc,
+    )(([pattern, value, flags]) => {
+      const result = new RegExp(pattern, flags).exec(value);
+      o(result ?? []);
+    });
+  };
