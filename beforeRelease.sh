56bad1b2e97f504930b714848af54d22e03a00ae
Eugene Kosukhin <kosuha606@gmail.com>
2025-08-15 18:50:44 +0300

    fix(main): before release script

--- beforeRelease.sh
+++ beforeRelease.sh
@@ -2,10 +2,10 @@
 
 current_branch=$(git rev-parse --abbrev-ref HEAD)
 
-if [ "$current_branch" != "release" ]; then
-  echo "Error: Current branch not 'release'. Release impossible you are on '$current_branch'."
+if [ "$current_branch" != "main" ]; then
+  echo "Error: Current branch not 'main'. Release impossible you are on '$current_branch'."
   exit 1
 fi
 
-echo "Current branch — 'release'."
+echo "Current branch — 'main'."
 exit 0
