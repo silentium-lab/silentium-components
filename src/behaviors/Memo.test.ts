849c5b334edf15fc86cacbda71d84c511b73e37c
Eugene Kosukhin <kosuha606@gmail.com>
2025-08-23 21:40:57 +0300

    fix(main): memo

--- src/behaviors/Memo.test.ts
+++ src/behaviors/Memo.test.ts
@@ -1,28 +1,30 @@
-import { of, shared } from "silentium";
+import { From, Late, Shared } from "silentium";
 import { expect, test, vi } from "vitest";
-import { memo } from "../behaviors/Memo";
+import { Memo } from "../behaviors/Memo";
 
 test("Memo.test", () => {
-  const [src, so] = of<number>(1);
-  const [mem] = shared(memo(src));
+  const l = new Late<number>(1);
+  const mem = new Shared(new Memo(l));
   const g = vi.fn();
-  mem(g);
+  mem.value(new From(g));
   let counter = 0;
 
-  mem(() => {
-    counter += 1;
-  });
+  mem.value(
+    new From(() => {
+      counter += 1;
+    }),
+  );
 
-  so(2);
-  so(2);
-  so(2);
-  so(2);
-  so(2);
+  l.owner().give(2);
+  l.owner().give(2);
+  l.owner().give(2);
+  l.owner().give(2);
+  l.owner().give(2);
 
   expect(g).toHaveBeenLastCalledWith(2);
   expect(counter).toBe(2);
 
-  so(3);
+  l.owner().give(3);
 
   expect(g).toHaveBeenLastCalledWith(3);
   expect(counter).toBe(3);
--- src/behaviors/Memo.ts
+++ src/behaviors/Memo.ts
@@ -1,20 +1,26 @@
-import { InformationType, TheInformation } from "silentium";
+import { From, TheInformation, TheOwner } from "silentium";
 
 /**
  * Didn't respond if new value of baseSrc equals to old value
  * https://silentium-lab.github.io/silentium-components/#/behaviors/memo
  */
-export const memo = <T>(baseSrc: InformationType<T>): InformationType<T> => {
-  let lastValue: T | null = null;
+export class Memo<T> extends TheInformation<T> {
+  public constructor(private baseSrc: TheInformation<T>) {
+    super(baseSrc);
+  }
 
-  return (o) => {
-    baseSrc((v) => {
-      if (v !== lastValue) {
-        o(v);
-        lastValue = v;
-      }
-    });
-  };
-};
+  public value(o: TheOwner<T>): this {
+    let lastValue: T | null = null;
 
-export class Memo<T> extends TheInformation<T> {}
+    this.baseSrc.value(
+      new From((v) => {
+        if (v !== lastValue) {
+          o.give(v);
+          lastValue = v;
+        }
+      }),
+    );
+
+    return this;
+  }
+}
