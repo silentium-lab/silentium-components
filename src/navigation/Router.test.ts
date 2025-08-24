b577ba9078f92bc97d7e4aba3befcf20331af24d
Eugene Kosukhin <kosuha606@gmail.com>
2025-08-24 16:35:27 +0300

    fix(main): aLl done

--- src/navigation/Router.test.ts
+++ src/navigation/Router.test.ts
@@ -1,46 +1,46 @@
-import { applied, i, of, shared } from "silentium";
+import { Applied, From, Late, Of, Shared } from "silentium";
 import { expect, test, vi } from "vitest";
-import { router } from "../navigation/Router";
+import { Router } from "../navigation/Router";
 
 const drop = (dropPart: string) => (value: string) => {
   return value.replace(dropPart, "");
 };
 
 test("Router.test", () => {
-  const [urlSrc, uo] = of<string>("http://domain.com/");
-  const [urlPathSrc] = shared(applied(urlSrc, drop("http://domain.com")));
+  const urlSrc = new Late<string>("http://domain.com/");
+  const urlPathSrc = new Shared(new Applied(urlSrc, drop("http://domain.com")));
   const g = vi.fn();
-  urlPathSrc(g);
+  urlPathSrc.value(new From(g));
 
-  const routerSrc = router(
+  const routerSrc = new Router(
     urlPathSrc,
-    i([
+    new Of([
       {
         pattern: "^/$",
-        template: i("page/home.html"),
+        template: new Of("page/home.html"),
       },
       {
         pattern: "/some/contacts",
         template: "page/contacts.html",
       },
     ]),
-    i("page/404.html"),
+    new Of<string>("page/404.html"),
   );
   const g2 = vi.fn();
-  routerSrc(g2);
+  routerSrc.value(new From(g2));
 
   expect(g2).toHaveBeenLastCalledWith("page/home.html");
 
-  uo("http://domain.com/some/contacts");
+  urlSrc.owner().give("http://domain.com/some/contacts");
 
   expect(g).toHaveBeenLastCalledWith("/some/contacts");
   expect(g2).toHaveBeenLastCalledWith("page/contacts.html");
 
-  uo("http://domain.com/some/unknown/");
+  urlSrc.owner().give("http://domain.com/some/unknown/");
 
   expect(g2).toHaveBeenLastCalledWith("page/404.html");
 
-  uo("http://domain.com/");
+  urlSrc.owner().give("http://domain.com/");
 
   expect(g2).toHaveBeenLastCalledWith("page/home.html");
 });
--- src/navigation/Router.ts
+++ src/navigation/Router.ts
@@ -1,39 +1,46 @@
-import { any, chain, i, InformationType, OwnerType } from "silentium";
-import { branch } from "../behaviors";
-import { regexpMatched } from "../system/RegexpMatched";
+import { Any, Chain, From, Of, TheInformation, TheOwner } from "silentium";
+import { Branch } from "../behaviors";
+import { RegexpMatched } from "../system";
 
 export interface Route<T> {
   pattern: string;
   patternFlags?: string;
-  template: T | InformationType<T>;
+  template: T | TheInformation<T>;
 }
 
 /**
  * Router component what will return template if url matches pattern
  * https://silentium-lab.github.io/silentium-components/#/navigation/router
  */
-export const router = <T = "string">(
-  urlSrc: InformationType<string>,
-  routesSrc: InformationType<Route<T>[]>,
-  defaultSrc: InformationType<T>,
-): InformationType<T> => {
-  return (o) => {
-    routesSrc((routes) => {
-      any(
-        chain(urlSrc, defaultSrc),
-        ...routes.map((r) => {
-          return branch(
-            regexpMatched(
-              i(r.pattern),
-              urlSrc,
-              r.patternFlags ? i(r.patternFlags) : undefined,
-            ),
-            (typeof r.template === "function"
-              ? r.template
-              : i(r.template)) as InformationType,
-          );
-        }),
-      )(o as OwnerType<unknown>);
-    });
-  };
-};
+export class Router<T = "string"> extends TheInformation<T> {
+  public constructor(
+    private urlSrc: TheInformation<string>,
+    private routesSrc: TheInformation<Route<T>[]>,
+    private defaultSrc: TheInformation<T>,
+  ) {
+    super(urlSrc, routesSrc, defaultSrc);
+  }
+
+  public value(o: TheOwner<T>): this {
+    this.routesSrc.value(
+      new From((routes) => {
+        new Any(
+          new Chain(this.urlSrc, this.defaultSrc),
+          ...routes.map((r) => {
+            return new Branch(
+              new RegexpMatched(
+                new Of(r.pattern),
+                this.urlSrc,
+                r.patternFlags ? new Of(r.patternFlags) : undefined,
+              ),
+              (r.template instanceof TheInformation
+                ? r.template
+                : new Of(r.template)) as TheInformation,
+            );
+          }),
+        ).value(o as TheOwner);
+      }),
+    );
+    return this;
+  }
+}
