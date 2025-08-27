2dcf8af1501719faa9b8ff56f19cae11506d9070
Eugene Kosukhin <kosuha606@gmail.com>
2025-08-27 21:30:14 +0300

    build: before release

--- dist/silentium-components.d.ts
+++ dist/silentium-components.d.ts
@@ -175,7 +175,7 @@ declare class Template extends TheInformation<string> {
 interface Route<T> {
     pattern: string;
     patternFlags?: string;
-    template: T | TheInformation<T>;
+    template: T | InformationType<T>;
 }
 /**
  * Router component what will return template if url matches pattern
